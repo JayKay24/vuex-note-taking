7aa86997e4ecfa06ad3db36daced456b59b10edd
James Kinyua <jameskinyua590@gmail.com>
2018-10-17 11:58:28 +0300

    chore(mutations): Add state mutations to mutate the state object

--- app/main.js
+++ app/main.js
@@ -1,3 +1,19 @@
+const state = {
+  notes: [],
+  timestamps: []
+}
+
+const mutations = {
+  ADD_NOTE(state, payload) {
+    let newNote = payload;
+    state.notes.push(newNote);
+  },
+  ADD_TIMESTAMP(state, payload) {
+    let newTimestamp = payload
+    state.timestamps.push(newTimestamp)
+  }
+}
+
 const inputComponent = {
   template: `<input placeholder='Enter a note' class="input is-small" type="text" />`,
 }
