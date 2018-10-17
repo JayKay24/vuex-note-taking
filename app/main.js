f8af924a12b8f45857d01f3b77f868dfc931f945
James Kinyua <jameskinyua590@gmail.com>
2018-10-17 13:34:17 +0300

    chore(Note count): Register a note count component inside the Vue instance

--- app/main.js
+++ app/main.js
@@ -37,13 +37,45 @@ const store = new Vuex.Store({
 })
 
 const inputComponent = {
-  template: `<input placeholder='Enter a note' class="input is-small" type="text" />`,
+  template: `
+    <input placeholder='Enter a note'
+      v-model="input" 
+      @keyup.enter="monitorEnterKey"
+      class="input is-small" type="text" />
+    `,
+  data() {
+    return {
+      input: ''
+    }
+  },
+  methods: {
+    monitorEnterKey() {
+      this.$store.dispatch('addNote', this.input)
+      this.$store.dispatch('addTimestamp', new Date().toLocaleString())
+      this.input = ''
+    }
+  }
+
+}
+
+const noteCountComponent = {
+  template: `
+    <div class="note-count">
+      Note count: {{ noteCount }}
+    </div>
+  `,
+  computed: {
+    noteCount() {
+      this.$store.getters.getNoteCount
+    }
+  }
 }
 
 new Vue({
   el: '#app',
   store,
   components: {
-    'input-component': inputComponent
+    'input-component': inputComponent,
+    'note-count-component': noteCountComponent
   }
 })
